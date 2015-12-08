bc8bc4d33afba468520d475a623742b99f14d94b
xu561865 <xuyi561865@sina.com>
2015-12-08 17:23:34 +0800

    添加 UIHASH 测试数据

---
+++ Test2/Object.cpp
@@ -0,0 +1,9 @@
+//
+//  Object.cpp
+//  Test2
+//
+//  Created by xuyi on 15/12/8.
+//  Copyright © 2015年 xuyi. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ Test2/Object.h
@@ -0,0 +1,13 @@
+//
+//  Object.h
+//  Test2
+//
+//  Created by xuyi on 15/12/8.
+//  Copyright © 2015年 xuyi. All rights reserved.
+//
+
+#ifndef Object_h
+#define Object_h
+
+
+#endif /* Object_h */
