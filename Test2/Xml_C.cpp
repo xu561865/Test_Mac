ea8d0eaed3e08aab2fd162790f3ef53c60db4765
xu561865 <xuyi561865@sina.com>
2015-12-16 16:47:07 +0800

    Test tinyxml2

---
+++ Test2/Xml_C.cpp
@@ -0,0 +1,11 @@
+//
+//  Xml_C.cpp
+//  Test2
+//
+//  Created by xuyi on 15/12/16.
+//  Copyright © 2015年 xuyi. All rights reserved.
+//
+
+#include "Xml_C.h"
+
+
---
+++ Test2/Xml_C.h
@@ -0,0 +1,18 @@
+//
+//  Xml_C.h
+//  Test2
+//
+//  Created by xuyi on 15/12/16.
+//  Copyright © 2015年 xuyi. All rights reserved.
+//
+
+#ifndef Xml_C_h
+#define Xml_C_h
+
+class Xml_C
+{
+public:
+    
+};
+
+#endif /* Xml_C_h */
